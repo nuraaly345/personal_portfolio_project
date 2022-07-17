617d7d267ba068374920e791e596079d376dd87c
nuraaly345 <kubanura57@gmail.com>
2022-07-17 16:43:36 +0300

    detail

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path, include
-from  . views import index
+from  . views import *
+
+app_name = 'blog'
 
 urlpatterns = [
     path('', index, name='index'),
+    path('<int:blog_id>/', detail, name='detail')
 ]
     
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from . models import Blog
 
 def index(request):
     blogs = Blog.objects.order_by('-blog_time')[:5]
 
     return render(request, 'blog/index.html', {'blogs': blogs})
+
+def detail(request, blog_id):
+    blog = get_object_or_404(Blog, pk=blog_id)
+    return render(request, 'blog/detail.html', {'blog': blog})
