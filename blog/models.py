e1c35dbc257b0c46104853aa0e580d14c8d978cb
nuraaly345 <kubanura57@gmail.com>
2022-07-18 13:49:01 +0300

    Калыптарды кенйтуу, стиль кошуу долбоорду аяктоо

--- blog/models.py
+++ blog/models.py
@@ -4,3 +4,6 @@ class Blog(models.Model):
     title = models.CharField(max_length=250)
     blog_time = models.DateField()
     description = models.TextField()
+
+    def __str__(self):
+        return self.title
